bbc9340d419d465a90c5a33f0fcbe399f0fa7609
Desarrollo2Fianza <dev@fianzabpo.com.co>
2023-02-15 08:18:39 -0500

    modularizar el manejo de rutas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,67 @@
+import { NgModule } from '@angular/core';
+
+/**
+ * Necessary package to get the 
+ * routing functionality.
+ */
+import { RouterModule, Routes } from "@angular/router";
+
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+/**
+ * +-----------------------+
+ * | Applicaction's ROUTES |
+ * +-----------------------+
+ * 
+ * Definir las rutas de la 
+ * aplicación.
+ */
+const appRoutes: Routes = [
+  {path: 'crisis-center', component: CrisisListComponent},
+  {path: 'heroes', component: HeroListComponent},
+
+  /**
+   * Para todas las rutas vacías redirigir 
+   * a la ruta en @redirectTo
+   */
+  {path: '', redirectTo: "/heroes", pathMatch: 'full'},
+
+  /**
+   * +------------------+
+   * | MATCH ALL ROUTES |
+   * +------------------+
+   * 
+   * Los doble astericos hacen match con todas las url's,
+   * por lo que este componente puede ser cargado con cualquier
+   * url. 
+   * 
+   * Por eso debe ser el último, ya que Angular hace
+   * match con la primera ruta que coindida en el array de Routes.
+   */
+  {path: '**', component: PageNotFoundComponent},
+];
+
+@NgModule({
+  imports: [
+    /**
+     * El método retorna un módulo que contiene
+     * el @Router service provider configurado.
+     * Permitiendo así la navegación.
+     * 
+     * Al estar ubicado en imports de AppModule, este
+     * array hace disponible el @Router for all app.
+     */
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+  ],
+
+  /**
+   * Hace disponible para 
+   * otros este módulo.
+   */
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,49 +2,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-/**
- * Necessary package to get the 
- * routing functionality.
- */
-import { RouterModule, Routes } from "@angular/router";
-
 import { AppComponent } from './app.component';
+
+/**
+ * Para hacer uso de las rutas hay
+ * que importar app-routing.module.ts
+ */
+import { AppRoutingModule } from './app-routing.module';
+
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-/**
- * +-----------------------+
- * | Applicaction's ROUTES |
- * +-----------------------+
- * 
- * Definir las rutas de la 
- * aplicación.
- */
-const appRoutes: Routes = [
-  {path: 'crisis-center', component: CrisisListComponent},
-  {path: 'heroes', component: HeroListComponent},
-
-  /**
-   * Para todas las rutas vacías redirigir 
-   * a la ruta en @redirectTo
-   */
-  {path: '', redirectTo: "/heroes", pathMatch: 'full'},
-
-  /**
-   * +------------------+
-   * | MATCH ALL ROUTES |
-   * +------------------+
-   * 
-   * Los doble astericos hacen match con todas las url's,
-   * por lo que este componente puede ser cargado con cualquier
-   * url. 
-   * 
-   * Por eso debe ser el último, ya que Angular hace
-   * match con la primera ruta que coindida en el array de Routes.
-   */
-  {path: '**', component: PageNotFoundComponent},
-];
 
 @NgModule({
   declarations: [
@@ -56,15 +24,13 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+
     /**
-     * El método retorna un módulo que contiene
-     * el @Router service provider configurado.
-     * Permitiendo así la navegación.
-     * 
-     * Al estar ubicado en imports de AppModule, este
-     * array hace disponible el @Router for all app.
+     * Se importa en este array
+     * para hacer uso de él, a través de
+     * toda la app.
      */
-    RouterModule.forRoot(appRoutes, {enableTracing: true})
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
